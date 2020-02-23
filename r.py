5a51cc726bd3db5340fcbdc76076bb5d2b12f38d
Mike LEUNG <ckleungah@gmail.com>
2020-02-23 15:35:06 +0800

    second version

--- r.py
+++ r.py
@@ -6,8 +6,15 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('File has been read over, there are total', len(data), 'records')
 
 print(data[0])
 print('---------------')
 print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('the average length is', sum_len/len(data))
